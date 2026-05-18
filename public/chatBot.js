7e394ae7b5fa806e60e477c6a7c38cacefde4aff
Neerajkumar199 <145154329+Neerajkumar199@users.noreply.github.com>
2026-05-18 14:15:07 +0530

    Update chatBot.js

--- public/chatBot.js
+++ public/chatBot.js
@@ -1,7 +1,7 @@
 (function () {
 
     const api_Url = "https://ai-customer-support-chatbot-taupe.vercel.app"
-     // const api_Url = "http://localhost:3000/api/chat"
+    
 
     const scriptTag = document.currentScript;
     const ownerId = scriptTag.getAttribute("data-owner-id")
